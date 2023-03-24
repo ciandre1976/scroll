bc7353636e48aadfc8340ee8974d102addcac291
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-03-24 20:09:38 +0100

    aded fetch

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,25 +1,48 @@
 import { useState, useEffect } from "react";
 import SelectElement from "./Comps/SelectElement";
+import axios from "axios";
 import "./App.css";
+import { useQuery } from "react-query";
+
+// Create a client
 
 function App() {
   const [state, setState] = useState("");
+  let url_rep = `http://localhost:3000/representatives/${state}`;
+  let url_sen = `http://localhost:3000/senators/${state}`;
+
+  const fetchRepresentatives = async () => {
+    const response = await axios.get(url_rep);
+    return response;
+  };
+
+  console.log(url_rep);
+  console.log(url_sen);
+  let res;
+
+  const { data } = useQuery("representatives", fetchRepresentatives);
+  console.log(data);
+  data?.isLoading ? <p>Loading...</p> : (res = data?.data?.results);
 
   const handleStateChange = (e) => {
     setState(e.target.value);
   };
-
-  useEffect(() => {
-    console.log("refetch...");
-    return () => {};
-  }, [state]);
-
-  console.log(state);
+  console.log("data je", res);
 
   return (
     <div className="App">
       <SelectElement handleStateChange={handleStateChange} />
-      <p>{state}</p>
+      {res?.map((person) => (
+        <article>
+          <p>{person?.district}</p>
+          <p>{person?.name}</p>
+          <p>{person?.link}</p>
+          <p>{person?.office}</p>
+          <p>{person?.party}</p>
+          <p>{person?.phone}</p>
+          <p>{person?.state}</p>
+        </article>
+      ))}
     </div>
   );
 }
