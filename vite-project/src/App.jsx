24a1d5578dcd4547da4099b0355572968c79b70a
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-03-26 16:11:17 +0200

    fix

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -9,6 +9,7 @@ import Person from "./Comps/Person";
 
 function App() {
   let [state, setState] = useState("");
+  let [dataRep, setDataRep] = useState([]);
   let url_rep = `http://localhost:3000/representatives/${state}`;
   let url_sen = `http://localhost:3000/senators/${state}`;
 
@@ -17,32 +18,12 @@ function App() {
     return response;
   };
 
-  useEffect(() => {
-    const onScroll = (e) => {
-      let fetching = false;
-      const { scrollHeight, scrollTop, clientHeight } =
-        e.target.scrollingElement;
-      if (!fetching && scrollHeight - scrollTop <= clientHeight * 1.2) {
-        fetching = true;
-        console.log("hi");
-        fetching = false;
-      }
-    };
-    document.addEventListener("scroll", onScroll);
-    return () => {
-      document.removeEventListener("scroll", onScroll);
-    };
-  }, [state]);
-
-  let { data, isLoading } = useQuery("representatives", fetchRepresentatives);
-  console.log(data);
-  let res;
-
-  data?.isLoading ? <p>Loading...</p> : (res = data?.data?.results);
+ 
   const handleStateChange = (e) => {
     setState(e.target.value);
   };
   console.log(state);
+
   return (
     <div className="App">
       <SelectElement handleStateChange={handleStateChange} />
